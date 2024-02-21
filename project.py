cd35aff1af64bb9d491f1882a2647decb164c1ce
Alexander Catchings <hieuviet0711@gmail.com>
2024-02-21 15:05:05 -0600

    Create project.py

---
+++ project.py
@@ -0,0 +1,39 @@
+'''
+Report 
+
+Submit the report as a group
+
+3 students maximum per project
+
+Report Format
+
+PDF
+
+5-10 pages (not more than 10 pages)
+
+Use 1.5 spacing, one column, 1 inch or less on each side
+The front page must have the title of the project and the name of the student
+
+Data description
+
+Data Source Link & Overview
+
+Column and data type - brief description
+
+Exploratory Data Analysis
+
+    Meaningful interpretations from the data
+
+Feature Engineering
+
+Explanation of the steps followed in cleaning and reducing the dimensionality of the data
+
+Code
+
+Notebooks (preferable) - step-by-step cleaning & visualization
+
+If you do not provide a notebook, please submit your code along with the data
+
+Add all instructions to run the code and get the output of what is in your report
+
+'''
